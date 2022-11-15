ef41883c0a55d7d2181c8cb7126d3cda3892458b
Rémino Rem <29999+remino@users.noreply.github.com>
2022-11-15 23:57:01 +0900

    Add _cmd_exists_alt

--- sh/cmd-exists.sh
+++ sh/cmd-exists.sh
@@ -4,7 +4,12 @@ _cmd_exists() {
 	which "$1" > /dev/null 2>&1
 }
 
+_cmd_exists_alt() {
+	command -v "$1" >/dev/null 2>&1
+}
+
 for i in ls doesnotexist
 do
-	echo "$i $( _cmd_exists "$i"; echo $? )"
+	echo "_cmd_exists $i: $( _cmd_exists "$i"; echo $? )"
+	echo "_cmd_exists_alt $i: $( _cmd_exists_alt "$i"; echo $? )"
 done
