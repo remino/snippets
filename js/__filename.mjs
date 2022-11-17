74fa04fac9a34905c18d2637832ed9daf0a35137
Rémino Rem <29999+remino@users.noreply.github.com>
2022-11-17 15:27:25 +0900

    Add js/__filename.mjs

---
+++ js/__filename.mjs
@@ -0,0 +1,7 @@
+// For Node ES Module.
+
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
