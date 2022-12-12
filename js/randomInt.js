b46d0579bb7f38fe35d94a4183fb786ade28ac6c
Rémino Rem <29999+remino@users.noreply.github.com>
2022-12-12 14:57:37 +0900

    Refactor randomInt.js

--- js/randomInt.js
+++ js/randomInt.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+// -- 8< -- 8< -- 8< -- 8< -- 8< -- 8< -- 8< -- 8< -- 8< -- 8< -- 8< -- 8< -- 8<
+
 export const randomInt = (a, b = 0) => {
 	const {
 		floor, random, max, min,
@@ -8,3 +10,17 @@ export const randomInt = (a, b = 0) => {
 	const result = floor(random() * floor(maxVal - minVal + 1)) + minVal
 	return result
 }
+
+// -- 8< -- 8< -- 8< -- 8< -- 8< -- 8< -- 8< -- 8< -- 8< -- 8< -- 8< -- 8< -- 8<
+
+const test = () => {
+	const min = 5
+	const max = 6
+	Array(1000).fill().forEach(i => {
+		const int = randomInt(min, max)
+		if (int < min || int > max) throw new Error('randomInt failed.')
+	})
+	console.info('All good!')
+}
+
+test()
