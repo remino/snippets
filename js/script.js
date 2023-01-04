24a2cf594d9fe1cdc8a28d7e3418b43b835aa77e
Rémino Rem <29999+remino@users.noreply.github.com>
2023-01-04 16:09:17 +0900

    Add error handling in script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
 #!/usr/bin/env node
+
+const getErrorMessageForCode = code => {
+	switch (code) {
+		default: return ''
+	}
+}
+
+class ConsoleError extends Error {
+	constructor(code, customMsg) {
+		super(customMsg || getErrorMessageForCode(code))
+		this.name = 'ConsoleError'
+		this.code = code
+	}
+}
+
+const main = async (...args) => {
+	console.log(args)
+}
+
+main(...process.argv.slice(2)).then(result => {
+	process.stdout.write(result)
+	process.exit(0)
+}).catch(e => {
+	process.stderr.write(e.message)
+	process.exit(e.code || 1)
+})
