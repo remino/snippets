230e790f4fbfd3c6b395959f11b5f4fb6b5f9380
Rémino Rem <29999+remino@users.noreply.github.com>
2023-01-06 17:48:52 +0900

    Add jsdoc.js

---
+++ js/jsdoc.js
@@ -0,0 +1,10 @@
+// https://jsdoc.app
+
+/**
+ * Display arguments in the console.
+ * @param {...*} args - The arguments to display.
+ * @returns {void}
+ */
+const echo = (...args) => {
+	console.log(...args)
+}
