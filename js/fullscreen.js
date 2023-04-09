e3658d55930d50bf96338e1d9143ed2843b54a07
Rémino Rem <29999+remino@users.noreply.github.com>
2023-04-10 07:55:40 +0900

    Add fullscreen.js

---
+++ js/fullscreen.js
@@ -0,0 +1,11 @@
+const goFullscreen = el => {
+	if (el.requestFullscreen) {
+		el.requestFullscreen()
+	} else if (el.mozRequestFullScreen) { /* Firefox */
+		el.mozRequestFullScreen()
+	} else if (el.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+		el.webkitRequestFullscreen()
+	} else if (el.msRequestFullscreen) { /* IE/Edge */
+		el.msRequestFullscreen()
+	}
+}
