864a0f719c382772c64f9aa78a2974d6f2589519
Rémino Rem <29999+remino@users.noreply.github.com>
2023-04-12 18:04:08 +0900

    Fix typo in comment

--- sh/1x1.sh
+++ sh/1x1.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-# Convert cannonical dump of a 1x1 transparent GIF to a GIF file using awk:
+# Convert canonical dump of a 1x1 transparent GIF to a GIF file using awk:
 awk '{for(i=2;i<17;i++) printf "%s", $i;}' << EOF | xxd -r -p > transparent.gif
 00000000  47 49 46 38 39 61 01 00  01 00 80 00 00 00 00 00  |GIF89a..........|
 00000010  ff ff ff 21 f9 04 01 00  00 00 00 2c 00 00 00 00  |...!.......,....|
 00000020  01 00 01 00 00 02 01 44  00 3b                    |.......D.;|
 EOF
 
-# Convert cannonical dump of a 1x1 transparent GIF to a GIF file using sed:
+# Convert canonical dump of a 1x1 transparent GIF to a GIF file using sed:
 sed -E 's/^[0-9]+//;s/\|.*$//;s/ //g' << EOF | xxd -r -p > transparent.gif
 00000000  47 49 46 38 39 61 01 00  01 00 80 00 00 00 00 00  |GIF89a..........|
 00000010  ff ff ff 21 f9 04 01 00  00 00 00 2c 00 00 00 00  |...!.......,....|
