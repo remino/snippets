92f9924b052953e1e3d66b669451636fac664ce9
Rémino Rem <29999+remino@users.noreply.github.com>
2023-07-02 16:02:22 +0900

    Fix _iso_date_to_sec & _sec_to_date_iso timezone

--- sh/date.sh
+++ sh/date.sh
@@ -13,9 +13,9 @@ iso_date_utc() {
 # Works for both GNU and BSD date.
 
 _iso_date_to_sec() {
-	{ date -j > /dev/null 2>&1 && date -j -f '%Y-%m-%dT%H:%M:%SZ' "$1" +%s; } || date --date="$1" +%s
+	{ date -j > /dev/null 2>&1 && date -j -f '%Y-%m-%dT%H:%M:%S%z' "$1" +%s; } || date --date="$1" +%s
 }
 
 _sec_to_date_iso() {
-	{ date -j > /dev/null 2>&1 && date -r "$1" +'%Y-%m-%dT%H:%M:%SZ'; } || date --date=@"$1" +'%Y-%m-%dT%H:%M:%SZ'
+	{ date -j > /dev/null 2>&1 && date -r "$1" +'%Y-%m-%dT%H:%M:%S%z'; } || date --date=@"$1" +'%Y-%m-%dT%H:%M:%S%z'
 }
