59facab035a52fb6e44bc880e51e1c35e96de51d
Rémino Rem <29999+remino@users.noreply.github.com>
2023-07-07 13:43:11 +0900

    Refactor case.sh, fix _to_lowercase_words

--- sh/case.sh
+++ sh/case.sh
@@ -1,31 +1,31 @@
 #!/bin/sh
 
 _to_lowercase() {
-	echo "$*" | tr '[:upper:]' '[:lower:]'
+	tr '[:upper:]' '[:lower:]'
 }
 
 _to_lowercase_words() {
-	echo "$*" | perl -pe 's/^./lc($&)/e;s/(?<![A-Z])([A-Z])/ \1/g;s/[^a-zA-Z0-9]+/ /g;s/(^[^a-zA-Z0-9]|[^a-zA-Z0-9]$)//g;s/.*/lc($&)/e'
+	perl -pe 's/(?<![A-Z])([A-Z])/ \1/g;s/[^a-zA-Z0-9]+/ /g;s/(^[^a-zA-Z0-9]|[^a-zA-Z0-9]$)//g;s/.*/lc($&)/e'
 }
 
 _to_camel_case() {
-	_to_lowercase_words "$*" | perl -pe 's/ [a-z]/uc($&)/ge;s/ ([A-Z])/\1/g'
+	_to_lowercase_words | perl -pe 's/ [a-z]/uc($&)/ge;s/ ([A-Z])/\1/g'
 }
 
 _to_pascal_case() {
-	_to_lowercase_words "$*" | perl -pe 's/(^| )[a-z]/uc($&)/ge;s/ ([A-Z])/\1/g'
+	_to_lowercase_words | perl -pe 's/(^| )[a-z]/uc($&)/ge;s/ ([A-Z])/\1/g'
 }
 
 _to_kebab_case() {
-	_to_lowercase_words "$*" | sed -E 's/ /-/g'
+	_to_lowercase_words | sed -E 's/ /-/g'
 }
 
 _to_snake_case() {
-	_to_lowercase_words "$*" | sed -E 's/ /_/g'
+	_to_lowercase_words | sed -E 's/ /_/g'
 }
 
 _to_uppercase() {
-	echo "$*" | tr '[:lower:]' '[:upper:]'
+	tr '[:lower:]' '[:upper:]'
 }
 
 _test() {
@@ -37,10 +37,11 @@ _test() {
 }
 
 ORIGINAL=" stringWith  various-separators in_IT! "
-_test "$( _to_lowercase_words "$ORIGINAL" )" "string with various separators in it"
-_test "$( _to_camel_case "$ORIGINAL" )" "stringWithVariousSeparatorsInIt"
-_test "$( _to_pascal_case "$ORIGINAL" )" "StringWithVariousSeparatorsInIt"
-_test "$( _to_kebab_case "$ORIGINAL" )" "string-with-various-separators-in-it"
-_test "$( _to_snake_case "$ORIGINAL" )" "string_with_various_separators_in_it"
-_test "$( _to_lowercase "$ORIGINAL" )" " stringwith  various-separators in_it! "
-_test "$( _to_uppercase "$ORIGINAL" )" " STRINGWITH  VARIOUS-SEPARATORS IN_IT! "
+_test "$( echo "$ORIGINAL" | _to_lowercase_words )" "string with various separators in it"
+_test "$( echo "$ORIGINAL" | _to_camel_case )" "stringWithVariousSeparatorsInIt"
+_test "$( echo "$ORIGINAL" | _to_pascal_case )" "StringWithVariousSeparatorsInIt"
+_test "$( echo "$ORIGINAL" | _to_kebab_case )" "string-with-various-separators-in-it"
+_test "$( echo "$ORIGINAL" | _to_snake_case )" "string_with_various_separators_in_it"
+_test "$( echo "$ORIGINAL" | _to_lowercase )" " stringwith  various-separators in_it! "
+_test "$( echo "$ORIGINAL" | _to_uppercase )" " STRINGWITH  VARIOUS-SEPARATORS IN_IT! "
+_test "$( echo "abc-1234" | _to_lowercase_words )" "abc 1234"
