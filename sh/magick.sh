783a57bd48ec38637b1959913a477974049ed554
Rémino Rem <29999+remino@users.noreply.github.com>
2023-08-12 10:42:17 +0900

    Tweak ImageMagick command snippet for Twitter card

--- sh/magick.sh
+++ sh/magick.sh
@@ -7,7 +7,7 @@ magick convert share.png -resize 1200x -gravity center -crop 1200x630+0+0 fb.png
 
 # Crop image for Twitter summary_large_image card, at 2:1 ratio:
 # https://developer.twitter.com/en/docs/twitter-for-websites/cards/overview/summary-card-with-large-image
-magick convert share.png -gravity center -crop 2880x1440+0+0 tw.png
+magick convert share.png -resize 2880x -gravity center -crop 2880x1440+0+0 tw.png
 
 # Montage of two images, side by side, with options for an image:
 magick montage \( -flop 26.jpg \) 01.jpg -geometry +0+0 banner.jpg
