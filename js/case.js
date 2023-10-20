58e51bc042e638e9e016b9db4c305db1e5f8ab52
Rémino Rem <29999+remino@users.noreply.github.com>
2023-10-20 15:18:52 +0900

    Update case.js with more tests and details

--- js/case.js
+++ js/case.js
@@ -2,12 +2,26 @@
 
 // Path: js/case.js
 
-const toKebabCase = str => str.replace(/[ _-]+/g, '-').replace(/([A-Z])/g, $1 => `-${$1.toLowerCase()}`)
-const toSnakeCase = str => str.replace(/[ _-]+/g, '_').replace(/([A-Z])/g, $1 => `_${$1.toLowerCase()}`)
+const toLowerCaseWords = str => str
+	.replace(/(?<![A-Z0-9])([A-Z0-9])/g, ' $1')
+	.replace(/[^a-zA-Z0-9]+/g, ' ')
+	.replace(/(^[^a-zA-Z0-9]|[^a-zA-Z0-9]$)/g, '')
+	.toLowerCase()
+
+const toKebabCase = str => toLowerCaseWords(str).replace(/ /g, '-')
+const toSnakeCase = str => toLowerCaseWords(str).replace(/ /g, '_')
 
 Array.from([
+	[toLowerCaseWords, '1234abc', '1234 abc'], // XXX
+	[toLowerCaseWords, 'ThisIsATest', 'this is a test'], // XXX
+	[toLowerCaseWords, 'abc-1234', 'abc 1234'],
+	[toLowerCaseWords, ' abc-1234 ', 'abc 1234'],
+	[toLowerCaseWords, ' abc1234 ', 'abc 1234'],
+	[toLowerCaseWords, ' ABC1234 ', 'abc 1234'], // XXX
+	[toLowerCaseWords, ' ABC-1234 ', 'abc 1234'],
 	[toKebabCase, 'camelCase', 'camel-case'],
 	[toKebabCase, 'test test', 'test-test'],
+	[toKebabCase, 'HTMLElement', 'html-element'], // XXX
 	[toSnakeCase, 'camelCase', 'camel_case'],
 	[toSnakeCase, 'test test', 'test_test'],
 ]).forEach(([func, input, expected]) => {
@@ -17,6 +31,6 @@ Array.from([
 		console.info(`OK: ${func.name}(${input}) = ${expected}`)
 	} else {
 		// eslint-disable-next-line no-console
-		console.error(`FAILED: ${func.name}(${input}) = ${expected} != ${result}`)
+		console.error(`FAILED: ${func.name}(${input}) = ${result} != ${expected}`)
 	}
 })
