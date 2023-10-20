ae60412186581eabdf409329e7b67f128e81c576
Rémino Rem <29999+remino@users.noreply.github.com>
2023-10-20 14:38:55 +0900

    Add time zones in date.sql

--- sql/date.sql
+++ sql/date.sql
@@ -1,7 +1,17 @@
-#!/usr/bin/env mysql
-
 -- UTC ISO date
+SET @prev_time_zone = @@time_zone;
+SET time_zone = '+00:00';
 SELECT DATE_FORMAT(NOW(), '%Y-%m-%dT%TZ');
+SET time_zone = @prev_time_zone;
 
 -- UTC ISO date without special chars
-SELECT DATE_FORMAT(NOW(), '%Y%m%d%T');
+SET @prev_time_zone = @@time_zone;
+SET time_zone = '+00:00';
+SELECT DATE_FORMAT(NOW(), '%Y%m%d%H%i%S');
+SET time_zone = @prev_time_zone;
+
+-- Local ISO date
+SET @prev_time_zone = @@time_zone;
+SET time_zone = '+00:00';
+SELECT DATE_FORMAT(CONVERT_TZ(NOW(), @@time_zone, 'Asia/Tokyo'), '%Y-%m-%dT%T');
+SET time_zone = @prev_time_zone;
