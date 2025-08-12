c50ee954209747eeadfc9806fb28183f3b47c70d
Rémino Rem <29999+remino@users.noreply.github.com>
2025-08-12 21:03:16 +0900

    Format read.sh

--- snippets/sh/read.sh
+++ snippets/sh/read.sh
@@ -1,22 +1,19 @@
 #!/bin/sh
 
 read_from_stdin() {
-	while read -r line
-	do
+	while read -r line; do
 		echo "$line"
 	done
 }
 
 read_from_file() {
-	while read -r line
-	do
+	while read -r line; do
 		echo "$line"
-	done < "$1"
+	done <"$1"
 }
 
 read_from_stdin_or_file() {
-	if [ -t 0 ]
-	then
+	if [ -t 0 ]; then
 		read_from_file "$1"
 	else
 		read_from_stdin
